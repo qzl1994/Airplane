b3aa9bf1862589d79b8f15e0abebe4675bb7ad32
qzl1994 <305165598@qq.com>
2016-10-21 10:27:09 +0800

    修改过时类型成员

--- Assets/_Client/Scripts/ModeGame.cs
+++ Assets/_Client/Scripts/ModeGame.cs
@@ -1,5 +1,6 @@
 ﻿using UnityEngine;
 using System;
+using UnityEngine.SceneManagement;
 
 namespace GameClient
 {
@@ -11,8 +12,11 @@ namespace GameClient
         public void StartGame()
         {
 #pragma warning disable CS0618 // 类型或成员已过时
-            Application.LoadLevel("Game");
+            //Application.LoadLevel("Game");
+
 #pragma warning restore CS0618 // 类型或成员已过时
+
+            SceneManager.LoadScene("Game");
         }
     }
 }
--- Assets/_UI/Scripts/ExitGameButton.cs
+++ Assets/_UI/Scripts/ExitGameButton.cs
@@ -1,5 +1,6 @@
 ﻿using UnityEngine;
 using UnityEngine.EventSystems;
+using UnityEngine.SceneManagement;
 
 namespace Game
 {
@@ -8,8 +9,10 @@ namespace Game
         public void OnPointerClick(PointerEventData eventData)
         {
 #pragma warning disable CS0618 // 类型或成员已过时
-            Application.LoadLevel("Menu");//加载菜单场景
+            //Application.LoadLevel("Menu");//加载菜单场景
 #pragma warning restore CS0618 // 类型或成员已过时
+
+            SceneManager.LoadScene("Menu");
 
             Time.timeScale = 1f;          //恢复时间缩放
         }
